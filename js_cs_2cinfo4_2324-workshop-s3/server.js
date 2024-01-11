3d431f5011548f454c87f86d3edf68c41b1634fb
zied <zied.hack@gmail.com>
2024-01-11 21:54:23 +0100

    Node JS apps

---
+++ js_cs_2cinfo4_2324-workshop-s3/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 9090;
+
+import gameRoutes from './routes/game.js'; // importer le router du fichier routes/game.js
+
+app.use(express.json()); // Pour analyser (parsing) les requetes application/json
+
+// préfixe chaque route ici avec /game
+app.use('/game', gameRoutes); // Utiliser les routes créés
+
+app.listen(port, () => {
+  console.log(`Server running at http://127.0.0.1:${port}/`);
+});
